39ac99e6105e111d18e5e2872ef1733b3fae567f
Aziz Tlili M1 Miage <aziz.tlili900@gmail.com>
2026-06-26 12:32:57 +0000

    init: structure projet maven + classe principale

---
+++ exam-session2/src/main/java/com/abonnements/Main.java
@@ -0,0 +1,23 @@
+package com.abonnements;
+
+import com.abonnements.api.ApiServer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int port = 8080;
+
+        // on peut passer le port en argument: java -jar app.jar 9090
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Port invalide, utilisation du port par defaut: 8080");
+            }
+        }
+
+        System.out.println("Demarrage de Budget Tracker sur le port " + port + "...");
+        ApiServer server = new ApiServer();
+        server.start(port);
+    }
+}
